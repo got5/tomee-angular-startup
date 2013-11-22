a2dc8e5fb6d2812953a25c802ac55d96dd99d928
Krazar <krazarpwnu@gmail.com>
2013-11-22 19:09:18 +0100

    init

---
+++ src/main/java/org/superbiz/rest/service/BlogApplication.java
@@ -0,0 +1,13 @@
+package org.superbiz.rest.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * User: pierremarot
+ * Date: 22/11/2013
+ * Time: 18:54
+ */
+@ApplicationPath("api")
+public class BlogApplication extends Application {
+}
